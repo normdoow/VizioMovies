463ab7be4466082b1505141837d0f218631f5d22
normdoow <noahbragg@cedarville.edu>
2019-08-15 14:12:21 -0400

    Initial Commit

---
+++ VizioMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VizioMovies
+//
+//  Created by Noah Bragg on 8/15/19.
+//  Copyright © 2019 Noah Bragg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
